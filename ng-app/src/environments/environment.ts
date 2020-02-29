7036ae2d8d731e545e13b970e11e53b4181d2163
Praveenkumarreddy123 <praveenkumarreddy9894@gmail.com>
2020-02-29 15:53:51 +0530

    tender fliter

--- ng-app/src/environments/environment.ts
+++ ng-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   version: 'Dev',
-  // WebServiceUrl : 'http://localhost/projects/IndoUKB2B-api',
-  WebServiceUrl : 'https://betaindukpraveenfreelancer.000webhostapp.com/IndoUKB2B-api/IndoUKB2B-api/'
+  WebServiceUrl : 'http://localhost/projects/IndoUKB2B-api',
+  // WebServiceUrl : 'https://betaindukpraveenfreelancer.000webhostapp.com/IndoUKB2B-api/IndoUKB2B-api/'
 };
